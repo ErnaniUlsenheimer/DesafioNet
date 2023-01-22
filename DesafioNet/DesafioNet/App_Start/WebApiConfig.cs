438b075a6911b33f2371a1ce57f215585a6ad36d
ErnaniUlsenheimer <ernaniu@gmail.com>
2023-01-22 14:42:42 -0300

    Web API DesafioDotNet

--- DesafioNet/DesafioNet/App_Start/WebApiConfig.cs
+++ DesafioNet/DesafioNet/App_Start/WebApiConfig.cs
@@ -13,7 +13,9 @@ namespace DesafioNet
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/v1/{controller}/{id}",
